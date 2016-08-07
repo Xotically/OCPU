312063d71373285bb246b7f459e2ebe5129911ba
Rainzye <Rainzye@users.noreply.github.com>
2016-08-07 15:16:51 +0100

    stop PM from sending advertised messages (may break :?)

--- commands.js
+++ commands.js
@@ -251,12 +251,13 @@ exports.commands = {
 
 		if (!message) message = '|pm|' + user.getIdentity() + '|' + targetUser.getIdentity() + '|' + target;
 		if (message.includes('psim.us')) {
-			if (Rooms.get('staff')) {
-				Rooms.get('staff').add('|raw|<div class="broadcast-red">' + user.name + ' has advertised in a PM.</div>').update();
-			}
-			if (Rooms.get('upperstaff')) {
-				Rooms.get('upperstaff').add('|raw|<div class="broadcast-red">' + user.name + ' has advertised in a PM.</div>').update();
-			}
+		    if (Rooms.get('staff')) {
+		        Rooms.get('staff').add('|raw|<div class="broadcast-red">' + user.name + ' has advertised in a PM.</div>').update();
+		    }
+		    if (Rooms.get('upperstaff')) {
+			Rooms.get('upperstaff').add('|raw|<div class="broadcast-red">' + user.name + ' has advertised in a PM.</div>').update();
+		    }
+		    return false;
 		}
 		user.send(message);
 		if (targetUser !== user) targetUser.send(message);
