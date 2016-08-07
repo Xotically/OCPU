a617e8a235c5dbb663334a20fc705704eb35dff3
zellman01 <zellman01@gmail.com>
2016-08-07 08:43:36 -0400

    Error fix

--- commands.js
+++ commands.js
@@ -252,10 +252,10 @@ exports.commands = {
 		if (!message) message = '|pm|' + user.getIdentity() + '|' + targetUser.getIdentity() + '|' + target;
 		if (message.includes('psim.us')) {
 			if (Rooms.get('staff')) {
-				Rooms.get('staff').add('|raw|<div class="broadcast-red">'user.name + ' has advertised in a PM.</div>').update();
+				Rooms.get('staff').add('|raw|<div class="broadcast-red">' + user.name + ' has advertised in a PM.</div>').update();
 			}
 			if (Rooms.get('upperstaff')) {
-				Rooms.get('upperstaff').add('|raw|<div class="broadcast-red">'user.name + ' has advertised in a PM.</div>').update();
+				Rooms.get('upperstaff').add('|raw|<div class="broadcast-red">' + user.name + ' has advertised in a PM.</div>').update();
 			}
 		}
 		user.send(message);
