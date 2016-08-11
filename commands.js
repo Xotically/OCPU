51c57a42c23bdf21abcbe9f515a091fd0d6ff7ee
zellman01 <zellman01@gmail.com>
2016-08-11 12:38:05 -0400

    Add a change to staff modchat broadcast

--- commands.js
+++ commands.js
@@ -1755,8 +1755,10 @@ exports.commands = {
 		
 		if (room.modchat == "~") {
 			Rooms.get("staff").add("|raw|<div class=\"broadcast-red\"> " + user.name + " has set modchat to " + room.modchat + " in " + room.id + ".</div>");
+		} else if (room.modchat == '&' || room.modchat == '#' || room.modchat == '*' || room.modchat == '@' || room.modchat == '%' || room.modchat == '+' || room.modchat == 'autoconfirmed') {
+			Rooms.get("staff").add("|raw|<div class=\"broadcast-#999900\"> " + user.name + " has set modchat to " + room.modchat + " in " + room.id + ".</div>");
 		} else {
-			Rooms.get("staff").add("|raw|<div class=\"broadcast-yellow\"> " + user.name + " has set modchat to " + room.modchat + " in " + room.id + ".</div>");
+			Rooms.get("staff").add("|raw|<div class=\"broadcast-blue\" " + user.name + " has set modchat to " + room.modchat + " in " + room.id + ".</div>");
 		}
 		
 	},
