fd21dec5d239ec8bc7e414efeef3f3ebf5ed375b
Rainzye <Rainzye@users.noreply.github.com>
2016-10-16 22:39:45 +0100

    Explain unused variable + Spacing

--- mmo-plugins/mmo-base.js
+++ mmo-plugins/mmo-base.js
@@ -3,7 +3,7 @@
 let color = require('../config/color');
 const path = require('path');
 
-let amount; // ???
+let amount; // Needed else annoying undefined errors.
 
 function levelName(amount) {
 	let name = " level";
@@ -19,9 +19,10 @@ function defenceName(amount) {
 	let name = " defence";
 	return amount === 1 ? name : name + "";
 }
+
 exports.commands = {
 	// All Stats
-    allstats: function(target, room, user) {
+        allstats: function(target, room, user) {
 		if (!this.runBroadcast()) return;
 		if (!target) target = user.name;
 
@@ -29,6 +30,7 @@ exports.commands = {
 		const att = Db('attack').get(toId(target), 0);
 		const def = Db('defence').get(toId(target), 0);
 		let group = user.getIdentity().charAt(0);
+	    
 		this.sendReply("|raw|<font color=#948A88><hr>" + group +  "</font><font color=" + color(user.userid) + "><b>" + Tools.escapeHTML(target) + "</b></font> has <b>" + lvl + levelName(amount) + "</b>.");
 		this.sendReply("|raw|<font color=#948A88>" + group +  "</font><font color=" + color(user.userid) + "><b>" + Tools.escapeHTML(target) + "</b></font> has <b>" + att + attackName(amount) + "</b>.");
 		this.sendReply("|raw|<font color=#948A88>" + group +  "</font><font color=" + color(user.userid) + "><b>" + Tools.escapeHTML(target) + "</b></font> has <b>" + def + defenceName(amount) + "</b>.<hr>");
